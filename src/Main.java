263750f5ad68ee58f364904e26da18061da2bd85
Neto test <samarokova.a.g@bk.ru>
2022-11-15 22:12:12 +0300

    Калькулятор бонусов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        double balance = 100;
+        double depositing = 2563;
+        double bonus = depositing / 100;
+
+        if (depositing >= 1000) {
+            System.out.println("Ваш баланс: " + (bonus + depositing + balance + " рублей"));
+            System.out.println("Ваши бонусы: " + bonus + " рублей");
+        }
+        else {
+            System.out.println("Ваш баланс: " + (balance + depositing));
+            System.out.println("Ваши бонусы: 0 рублей");
+        }
+        }
 }
